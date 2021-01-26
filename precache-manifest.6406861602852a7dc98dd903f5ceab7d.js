7f85ab8095c97f2cf1ada1869f61ab88a5798e72
zeltmanis <zeltmanis@gmail.com>
2021-01-26 20:36:43 +0200

    Updates

---
+++ precache-manifest.6406861602852a7dc98dd903f5ceab7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2b54629f3f225cbcbae44b3ecafd8f",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "a65904401048a9fdd29d",
+    "url": "/test/static/css/main.7f77a147.chunk.css"
+  },
+  {
+    "revision": "4e72073b098c8be33f67",
+    "url": "/test/static/js/2.0e53755e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/test/static/js/2.0e53755e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a65904401048a9fdd29d",
+    "url": "/test/static/js/main.b037beeb.chunk.js"
+  },
+  {
+    "revision": "62b7fc61f4e21c8e578f",
+    "url": "/test/static/js/runtime-main.4dc19d97.js"
+  }
+]);
